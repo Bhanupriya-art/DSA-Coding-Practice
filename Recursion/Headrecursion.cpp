95b75c13743fc7aa8ed6fb2682b1b5c9d380ac0f
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-01-02 11:51:00 +0530

    Add files via upload

---
+++ Recursion/Headrecursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n){
+    if(n>0){
+        fun(n-1);
+        cout<<n<<" ";
+    }
+}
+
+int main(){
+    int x = 3;
+
+    fun(x);
+    return 0;
+} 
