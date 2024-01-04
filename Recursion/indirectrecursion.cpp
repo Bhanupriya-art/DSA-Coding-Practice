453146296e116aabbb29ef08f26a571f03562147
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-01-04 13:33:40 +0530

    Add files via upload

---
+++ Recursion/indirectrecursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+void funB(int n);
+void funA (int n)
+{
+    if(n>0)
+    {
+        printf("%d\n",n);
+        funB(n-1);
+    }
+}
+void funB(int n){
+    if(n>1)
+    {
+        printf("%d\n",n);
+        funA(n/2);
+    }
+}
+int main(){
+    funA(20);
+    return 0;
+}
