68e4a770d5178035ba32897657521ff0ad581b0f
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-01-06 18:25:17 +0530

    Add files via upload

---
+++ Recursion/sumofnusingrecursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int sum(int n){
+    if(n==0)
+    return 0;
+    return sum(n-1)+n;
+}
+int Isum(int n){
+    int s=0;
+    int i;
+    for(i=1;i<=n;i++)
+    s=s+i;
+    return s;
+}
+int main(){
+    int r;
+    r=Isum(5);
+    printf("%d",r);
+    return 0;
+
+}
