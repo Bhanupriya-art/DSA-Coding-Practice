43f009872872da55d494e28fa3a10ee367fd8662
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-01-08 15:27:07 +0530

    Add files via upload

---
+++ Recursion/powerrecursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int power(int m,int n){
+    if(n==0)
+    return 1;
+    return power(m,n-1)*m;
+}
+int power1(int m,int n)
+{
+    if(n==0)
+    return 1;
+    if(n%2==0)
+    return power1(m*m,n/2);
+    return m*power1(m*m,(n-1)/2);
+}
+int main(){
+    int r=power(2,9);
+    printf("%d",r);
+    return 0;
+
+}
