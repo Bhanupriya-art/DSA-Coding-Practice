65d41b3ebf4bee9b7a0a3180cbb46cf38454b42c
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-01-18 17:16:29 +0530

    Add files via upload

---
+++ Arrays Representation/Intro_To_Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int  main(){
+    int arr[5] = {1,2,3,4,5};
+    int arr2[5];
+    int arr3[5] = {1,2,3};
+    int arr4[5] = {0};
+    int arr5[] = {1,2,3,4,5,6,7,8,9,10};
+}
+
