a2ef095c4eb5d7392846213272a43b2a1de64638
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-02-19 14:20:45 +0530

    Add files via upload

---
+++ Arrays ADT/Insertion_In_Sorted_Array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+void display(struct Array arr){
+    int i;
+    cout << "The elements of an array: ";
+    for(i=0;i<arr.length;i++){
+        cout<<arr.A[i]<<" ";
+    }
+
+}
+
+void Insertsort(struct Array *arr, int x){
+    int i = arr->length-1;
+    if(arr->length==arr->size){
+        return;
+    }
+    while(i>=0 && arr->A[i]>x){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = x;
+    arr->length++;
+
+}
+
+int main(){
+    Array arr;
+    int n,i;
+    cout << "Enter the size of the array: ";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+    cout << "Enter the number of elements that should be present in the array: ";
+    cin>>n;
+    cout << "Enter the elements for an array: ";
+    for(i=0;i<n;i++){
+        cin>>arr.A[i];
+    }
+    arr.length = n;
+
+    display(arr);
+
+    int insertedelement;
+    cout << "\nEnter the element that should be inserted in the array: ";
+    cin >> insertedelement;
+
+    Insertsort(&arr,insertedelement);
+    display(arr);
+    
+
+}
