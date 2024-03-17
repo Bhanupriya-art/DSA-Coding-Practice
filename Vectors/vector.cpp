f3e4e8f6c19aea4ae5a6355bd1b6eb04f52b1648
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-03-17 14:11:02 +0530

    Add files via upload

---
+++ Vectors/vector.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v;
+
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.push_back(1);
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.push_back(2);
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.push_back(3);
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.resize(5);
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.resize(10);
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+    v.pop_back();
+    v.pop_back();
+    cout << "Size: "<<v.size()<<endl;
+    cout << "Capacity: "<<v.capacity()<<endl;
+
+}
